be4fca347041916b0c0846303405e9dab4551108
Blake Bless <blakebless7@gmail.com>
2016-09-28 14:46:01 -0700

    have different sections for projects

--- js/moves.js
+++ js/moves.js
@@ -1,3 +1,17 @@
+var y = $(window).scrollTop();
+
 $('.move-down').click(function() {
-  alert("YO");
+  $(window).scrollTop(y + 770);
+});
+
+$('.move-down2').click(function() {
+  $(window).scrollTop(y + 1540);
+});
+
+$('.move-down3').click(function() {
+  $(window).scrollTop(y + 2310);
+});
+
+$('.move-up').click(function() {
+  $(window).scrollTop();
 });
