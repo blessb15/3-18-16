7b2e1af42b83150e8be19047e0180820807974ee
Blake Bless <blakebless7@gmail.com>
2016-09-28 21:20:14 -0700

    fix more bugs

--- js/moves.js
+++ js/moves.js
@@ -1,17 +1,24 @@
 var y = $(window).scrollTop();
+var expressit = $('.expressit').height();
+var totallyserious = $('.totallyserious').height();
+var blocktalk = $('.blocktalk').height();
 
 $('.move-down').click(function() {
-  $(window).scrollTop(y + 770);
+  $(window).scrollTop($(window).height());
+  console.log("yo1");
 });
 
 $('.move-down2').click(function() {
-  $(window).scrollTop(y + 1540);
+  $(window).scrollTop($(window).height() * 2);
+  console.log("yo2");
 });
 
 $('.move-down3').click(function() {
-  $(window).scrollTop(y + 2310);
+  $(window).scrollTop($(window).height() * 3);
+  console.log("yo3");
 });
 
 $('.move-up').click(function() {
-  $(window).scrollTop();
+  $(window).scrollTop(totallyserious - blocktalk - expressit);
+  console.log("yo4");
 });
