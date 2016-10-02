f5f1957e49a69a6d54d05ed32c66cfbe05865794
Blake Bless <blakebless7@gmail.com>
2016-10-01 22:35:42 -0700

    have header with more contact info

--- js/moves.js
+++ js/moves.js
@@ -22,3 +22,15 @@ $('.move-up').click(function() {
   $(window).scrollTop(totallyserious - blocktalk - expressit);
   console.log("yo4");
 });
+
+///head-bar scroll
+$(window).scroll(function(){
+  if ($(window).scrollTop() < 730) {
+    $('.head-bar').addClass('head-bar-fixed');
+    console.log("do it 1");
+  }
+  if ($(window).scrollTop() > 731) {
+    console.log("do it 2");
+    $('.head-bar').removeClass('head-bar-fixed');
+  }
+});
