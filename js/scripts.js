b68b17c91d0824fb6f9a02a97e1602fe274da129
Blake Bless <blakebless7@gmail.com>
2016-10-04 21:11:26 -0700

    link up pages

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,24 @@ $(window).scroll(function(){
     $('.navigation').addClass('nav-fade');
     $('.logo').addClass('logo-shrink');
     $('.burger-icon').addClass('burger-shrink');
+    $('.minimize-icon').addClass('minimize-shrink');
   }
   if ($(window).scrollTop() < 100){
     $('.navigation').removeClass('nav-fade');
     $('.logo').removeClass('logo-shrink');
     $('.burger-icon').removeClass('burger-shrink');
+    $('.minimize-icon').removeClass('minimize-shrink');
   }
+});
+
+$('.nav-toggle-burger').click(function() {
+  console.log("hit");
+  $('.nav-toggle-burger').addClass('nav-hidden');
+  $('.nav-toggle-minimize').removeClass('nav-hidden');
+});
+
+$('.nav-toggle-minimize').click(function() {
+  console.log("hit2");
+  $('.nav-toggle-minimize').addClass('nav-hidden');
+  $('.nav-toggle-burger').removeClass('nav-hidden');
 });
