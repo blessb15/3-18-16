9d5dd1703994f8623a70da8bb2d5ac07382b96d6
Blake Bless <blakebless7@gmail.com>
2016-10-04 20:29:49 -0700

    have all new look

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,19 @@
 var y = $(window).scrollTop();
-var expressit = $('.expressit').height();
-var totallyserious = $('.totallyserious').height();
-var blocktalk = $('.blocktalk').height();
 
 $('.move-down').click(function() {
   $(window).scrollTop($(window).height());
   console.log("yo1");
 });
 
-$('.move-down2').click(function() {
-  $(window).scrollTop($(window).height() * 2);
-  console.log("yo2");
-});
-
-$('.move-down3').click(function() {
-  $(window).scrollTop($(window).height() * 3);
-  console.log("yo3");
-});
-
-$('.move-up').click(function() {
-  $(window).scrollTop(totallyserious - blocktalk - expressit);
-  console.log("yo4");
+$(window).scroll(function(){
+  if ($(window).scrollTop() >= 100){
+    $('.navigation').addClass('nav-fade');
+    $('.logo').addClass('logo-shrink');
+    $('.burger-icon').addClass('burger-shrink');
+  }
+  if ($(window).scrollTop() < 100){
+    $('.navigation').removeClass('nav-fade');
+    $('.logo').removeClass('logo-shrink');
+    $('.burger-icon').removeClass('burger-shrink');
+  }
 });
