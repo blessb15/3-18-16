4105eb5e437e30f438f52eefe25fdb613cc51355
Blake Bless <blakebless7@gmail.com>
2016-10-07 10:48:16 -0700

    be in final form for now

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,14 @@ $(window).scroll(function(){
     $('.logo').addClass('logo-shrink');
     $('.burger-icon').addClass('burger-shrink');
     $('.minimize-icon').addClass('minimize-shrink');
+    $('.nav-window').addClass('nav-window-fade');
   }
   if ($(window).scrollTop() < 100){
     $('.navigation').removeClass('nav-fade');
     $('.logo').removeClass('logo-shrink');
     $('.burger-icon').removeClass('burger-shrink');
     $('.minimize-icon').removeClass('minimize-shrink');
+    $('.nav-window').removeClass('nav-window-fade');
   }
 });
 
@@ -24,10 +26,15 @@ $('.nav-toggle-burger').click(function() {
   console.log("hit");
   $('.nav-toggle-burger').addClass('nav-hidden');
   $('.nav-toggle-minimize').removeClass('nav-hidden');
+  $('.nav-fixed').removeClass('nav-hidden');
+  $('.nav-window').show();
+
 });
 
 $('.nav-toggle-minimize').click(function() {
   console.log("hit2");
   $('.nav-toggle-minimize').addClass('nav-hidden');
+  $('.nav-fixed').addClass('nav-hidden');
   $('.nav-toggle-burger').removeClass('nav-hidden');
+  $('.nav-window').hide();
 });
