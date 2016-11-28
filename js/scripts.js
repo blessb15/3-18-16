4f0906f3f24bdbfc6a408b5ac6a15bd0c52428f5
blessb15 <blakebless7@gmail.com>
2016-11-28 11:55:30 -0800

    be a wip new look.

--- js/scripts.js
+++ js/scripts.js
@@ -9,89 +9,41 @@ $(document).ready(function(){
   $(window).scroll(function(){
     if ($(window).scrollTop() >= 100){
       $('.navigation').addClass('nav-fade');
-      $('.logo').addClass('logo-shrink');
-      $('.burger-icon').addClass('burger-shrink');
-      $('.minimize-icon').addClass('minimize-shrink');
-      $('.nav-window').addClass('nav-window-fade');
     }
     if ($(window).scrollTop() < 100){
       $('.navigation').removeClass('nav-fade');
-      $('.logo').removeClass('logo-shrink');
-      $('.burger-icon').removeClass('burger-shrink');
-      $('.minimize-icon').removeClass('minimize-shrink');
-      $('.nav-window').removeClass('nav-window-fade');
     }
   });
-
-  $('.nav-option').click(function(){
-    $('.nav-toggle-minimize').addClass('nav-hidden');
-    $('.nav-fixed').addClass('nav-hidden');
-    $('.nav-toggle-burger').removeClass('nav-hidden');
-    $('.nav-window').hide();
-  });
-
-  $('.nav-toggle-burger').click(function() {
-    $('.nav-toggle-burger').addClass('nav-hidden');
-    $('.nav-toggle-minimize').removeClass('nav-hidden');
-    $('.nav-fixed').removeClass('nav-hidden');
-    $('.nav-window').show();
-  });
-
-  $('.nav-toggle-minimize').click(function() {
-    $('.nav-toggle-minimize').addClass('nav-hidden');
-    $('.nav-fixed').addClass('nav-hidden');
-    $('.nav-toggle-burger').removeClass('nav-hidden');
-    $('.nav-window').hide();
-  });
-
-  $('.email-send').click(function(){
-    $('.contact').hide();
-    $('.projects').show();
-  });
-
-  var timeLoop = setTimeout(projectPicLoop, 5000);
-
-  var expressItImages = ['img/exp2.png', 'img/exp3.png', 'img/expressitpic.png'];
-  var thinkDifferetImages = ['img/TD2.png', 'img/TD3.png', 'img/thinkdifferent.png'];
-  var totallyseriousImages = ['img/TSP2.png', 'img/TSP3.png', 'img/TotallySerious.png'];
-
-  var int = 0;
-
-  function projectPicLoop(){
-    $('.expressit').css('background-image', 'url(' + expressItImages[int] + ')');
-    $('.thinkdifferent').css('background-image', 'url(' + thinkDifferetImages[int] + ')');
-    $('.totallyserious').css('background-image', 'url(' + totallyseriousImages[int] + ')');
-
-    int ++;
-
-    if (int > 2){
-      int = 0;
-    }
-
-    setTimeout(projectPicLoop, 5000);
-  }
 
   $('.expressit .image-overlay').hover(function() {
     $('.expressit .overlay-text').show();
+    $('.expressit-image').addClass('blur');
   }, function(){
     $('.expressit .overlay-text').hide();
+    $('.expressit-image').removeClass('blur');
   });
 
   $('.thinkdifferent .image-overlay').hover(function() {
     $('.thinkdifferent .overlay-text').show();
+    $('.thinkdifferent-image').addClass('blur');
   }, function(){
+    $('.thinkdifferent-image').removeClass('blur');
     $('.thinkdifferent .overlay-text').hide();
   });
 
   $('.totallyserious .image-overlay').hover(function() {
     $('.totallyserious .overlay-text').show();
+    $('.totallyserious-image').addClass('blur');
   }, function(){
+    $('.totallyserious-image').removeClass('blur');
     $('.totallyserious .overlay-text').hide();
   });
 
   $('.blocktalk .image-overlay').hover(function() {
     $('.blocktalk .overlay-text').show();
+    $('.blacktalk-image').addClass('blur');
   }, function(){
+    $('.blocktalk-image').removeClass('blur');
     $('.blocktalk .overlay-text').hide();
   });
 
