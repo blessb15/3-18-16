f25b72dc19c48df6588567d39c16273491e7554d
Blake Bless <blakebless7@gmail.com>
2016-12-30 20:23:40 -0800

    add new project

--- js/scripts.js
+++ js/scripts.js
@@ -16,13 +16,21 @@ $(document).ready(function(){
 
   });
 
-  $('.expressit .image-overlay').hover(function() {
-    $('.expressit .overlay-text').show();
-    $('.expressit-image').addClass('blur');
+  $('.chucksplace .image-overlay').hover(function() {
+    $('.chucksplace .overlay-text').show();
+    $('.chucksplace-image').addClass('blur');
   }, function(){
-    $('.expressit .overlay-text').hide();
-    $('.expressit-image').removeClass('blur');
+    $('.chucksplace-image').removeClass('blur');
+    $('.chucksplace .overlay-text').hide();
   });
+
+  // $('.expressit .image-overlay').hover(function() {
+  //   $('.expressit .overlay-text').show();
+  //   $('.expressit-image').addClass('blur');
+  // }, function(){
+  //   $('.expressit .overlay-text').hide();
+  //   $('.expressit-image').removeClass('blur');
+  // });
 
   $('.thinkdifferent .image-overlay').hover(function() {
     $('.thinkdifferent .overlay-text').show();
@@ -32,13 +40,13 @@ $(document).ready(function(){
     $('.thinkdifferent .overlay-text').hide();
   });
 
-  $('.totallyserious .image-overlay').hover(function() {
-    $('.totallyserious .overlay-text').show();
-    $('.totallyserious-image').addClass('blur');
-  }, function(){
-    $('.totallyserious-image').removeClass('blur');
-    $('.totallyserious .overlay-text').hide();
-  });
+  // $('.totallyserious .image-overlay').hover(function() {
+  //   $('.totallyserious .overlay-text').show();
+  //   $('.totallyserious-image').addClass('blur');
+  // }, function(){
+  //   $('.totallyserious-image').removeClass('blur');
+  //   $('.totallyserious .overlay-text').hide();
+  // });
 
   $('.blocktalk .image-overlay').hover(function() {
     $('.blocktalk .overlay-text').show();
